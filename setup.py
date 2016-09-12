2e16cc2f2014126e45281d044f4c8bafb129eb2a
Suraj Arya <suraj.p.arya@gmail.com>
2016-09-12 10:32:21 +0530

    * changed the setup.py a little bit to update on pypi

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 
 from setuptools import setup
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
 
 requirements = [
     "requests",
@@ -13,9 +11,9 @@ requirements = [
 
 setup(
     name='get-captcha',
-    version='0.1.0',
+    version='0.1.1',
     description="Get Captcha text from the image using real intelligence, not artificial intelligence. Its a client for get-captcha apis",
-    long_description=readme,
+    long_description='',
     author="Suraj Arya",
     author_email='suraj@loanzen.in',
     url='https://github.com/suraj-arya/captcha',
